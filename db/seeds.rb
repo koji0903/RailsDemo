873d029bdb611c930c1c47e7fd144eed49659a10
Koji HIJIKURO <koji0903@gmail.com>
2015-03-03 09:14:53 +0000

    Initialize data on Db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# Category
+literature = Category.create! name: "文学"
+history = Category.create! name: "歴史"
+business = Category.create! name: "ビジネス"
+science = Category.create! name: "科学"
+comic = Category.create! name: "コミック"
+
+# Publisher
+suzuki = Publisher.create! name: "スズキ書店"
+tanaka = Publisher.create! name: "田中出版"
+shugo = Publisher.create! name: "集合社"
+otona = Publisher.create! name: "大人館"
