05f7abb96f238af5a174dce666f17a205a05ccf8
Koji HIJIKURO <koji0903@gmail.com>
2015-03-03 09:38:49 +0000

    change form items

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :price, :released_on, :rating, :discontinued, :publisher_id)
+      params.require(:product).permit(:name, :price, :released_on, :rating, :discontinued, :publisher_id, category_ids: [])
     end
 end
